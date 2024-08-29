525849d4fd94b6e45339455917a0dd048cbda86a
danielmoya <damr67@gmail.com>
2024-08-29 12:22:35 -0400

    Add scheduling service boilerplate

---
+++ apps/scheduling-service/src/test/java/sm_saas/scheduling_service/SchedulingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sm_saas.scheduling_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
